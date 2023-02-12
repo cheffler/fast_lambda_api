02dbc80d6947fdd6d2862006aa0b6d56343d201a
James Leffler <james.leffler@constelcom.com>
2023-02-12 21:48:47 +0000

    feat: extending API spec example

--- example/basic/main.py
+++ example/basic/main.py
@@ -1,11 +1,34 @@
-from src import app
+from src import app, customise_openapi_schema
 from .routes.commands import (
     commands_api_v1,
     commands_api_v2,
     OPENAPI_TAG as command_tags,
 )
+from fastapi.staticfiles import StaticFiles
+from .desc import MAIN_DESCRIPTION
 
-app.openapi_tags = [command_tags]
+
+customise_openapi_schema(
+    title="Basic Example of Lambda API Gateway",
+    version="0.1.0",
+    description=MAIN_DESCRIPTION,
+    tags=[command_tags],
+    servers=[
+        {
+            "url": "http://localhost:8000",
+            "description": "Local server",
+        }
+    ],
+    logo_url="http://localhost:8000/static/logo_black.png",
+)
+
+# To enable the logo, mount a static directory
+app.mount(
+    "/static",
+    StaticFiles(directory="example/basic/static"),
+    name="static",
+)
+
 
 app.include_router(commands_api_v1)
 app.include_router(commands_api_v2)
