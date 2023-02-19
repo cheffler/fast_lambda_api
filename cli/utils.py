b8f4abc29a59ae478c8b7696730779c37550c2ec
James Leffler <james.leffler@constelcom.com>
2023-02-19 20:52:00 +0000

    chore: remove spec related utils, moved to spec

--- cli/utils.py
+++ cli/utils.py
@@ -1,14 +1,7 @@
-import json
 from importlib import import_module
 from types import ModuleType
 
-import yaml
-from click import UsageError
 from fastapi import FastAPI
-from pydantic import AnyUrl
-from yaml import Dumper
-
-from .config import config
 
 
 def load_app(module: str, app_name: str) -> FastAPI:
@@ -19,33 +12,3 @@ def load_app(module: str, app_name: str) -> FastAPI:
     handler_module: ModuleType = import_module(module)
 
     return getattr(handler_module, app_name)
-
-
-def any_url_representer(dumper: Dumper, data: AnyUrl):
-    """
-    yaml representer to handle the AnyUrl class
-    """
-    return dumper.represent_str(data)
-
-
-def format_spec(spec: dict, format: str):
-    """
-    Format the specification to the target format
-    """
-    if format == "json":
-        return json.dumps(
-            spec,
-            sort_keys=False,
-            indent=config.default_json_spaces,
-            default=str,
-        )
-
-    if format == "yaml":
-        yaml.add_representer(AnyUrl, any_url_representer)
-        return yaml.dump(
-            spec,
-            indent=config.default_yaml_spaces,
-            sort_keys=False,
-        )
-
-    raise UsageError(f"File format {format} is not supported")
