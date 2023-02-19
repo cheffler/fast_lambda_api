6f9f90b849f47b130b7f0e46b2d544bdb4517bd2
James Leffler <james.leffler@constelcom.com>
2023-02-19 20:51:47 +0000

    feat: add option to filter on versions

--- cli/main.py
+++ cli/main.py
@@ -1,11 +1,13 @@
 from os import makedirs
 from pathlib import Path
+from typing import List
 
 from click import Choice, echo
 from click.decorators import command, group, option
 
 from .config import config
-from .utils import format_spec, load_app
+from .spec import filter_paths_in_app, format_spec
+from .utils import load_app
 
 
 @group
@@ -49,28 +51,45 @@ def cli():
     default="yaml",
     help="The format of the outputted file",
 )
+@option(
+    "-v",
+    "--version",
+    type=str,
+    multiple=True,
+    help="Filter for these versions only",
+)
 def export_open_api_spec(
     target: str,
     app_name: str,
     output_dir: str,
     file_name: str,
     format: str,
+    version: List[str],
 ) -> None:
     """
     Export the OpenAPI specification from the target application
     """
     echo(f"reading app from {target}:{app_name}")
+    specs = {}
     app = load_app(target, app_name)
 
-    spec = app.openapi()
+    if version and len(version) > 0:
+        for v in version:
+            specs[v] = filter_paths_in_app(app, v)
+    else:
+        specs["default"] = app.openapi()
 
-    spec_str = format_spec(spec, format)
+    target_dir: Path = Path(".") / output_dir
+    makedirs(target_dir, exist_ok=True)
 
-    target_path = Path(".") / output_dir / f"{file_name}.{format}"
+    for name, spec in specs.items():
+        spec_str: str = format_spec(spec, format)
+        if name == "default":
+            filename = f"{file_name}.{format}"
+        else:
+            filename = f"{file_name}.{name}.{format}"
 
-    makedirs(target_path.parent, exist_ok=True)
-
-    target_path.write_text(spec_str)
+        (target_dir / filename).write_text(spec_str)
 
 
 cli.add_command(export_open_api_spec)
